66a419d607b7993ddde1f90e98fde7b33c926b94
Kristen Tidmuss <11579255+KristenTidmuss@users.noreply.github.com>
2022-02-06 17:28:54 +0000

    Switch to serverless cloudflare worker

---
+++ index.js
@@ -0,0 +1,53 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+//Branch for Nottingham Sherwood: 8a2d9772-268f-4d62-8291-f7bb28f10302
+async function handleRequest() {
+  //Get Data
+  const branch = await SETTING.get('branch')
+  const url = 'https://www.thegymgroup.com/BranchGymBusynessBlock/GetBusynessForBranch/?branchId='+branch+'&configurationId=0749f44e-4aa9-495d-a37a-d84a36d4b999'
+  const response = await fetch(url)
+  const results = await response.json()
+  //Assign Variables
+  const capacity = results['currentBranch']['capacity']
+  const updated = results['currentBranch']['lastUpdated']
+  const threshhold = results['currentBranch']['threshold']
+  //Determine status color
+  let color
+  switch (threshhold){
+    case 'lower':
+      color = 326144
+      break
+    case 'middle':
+      color = 16185856
+      break
+    default:
+      color = 16384000
+  }
+  //Post webhook
+  const webhook = await SETTING.get('webhook')
+  const data = {
+    "content": "",
+    "embeds": [
+      {
+        "title": capacity,
+        "color": color,
+        "timestamp": updated,
+      }
+    ]
+  }
+  const init = {
+    body: JSON.stringify(data),
+    method: "PATCH",
+    headers: {
+      "content-type": "application/json;charset=UTF-8",
+    },
+  }
+  const webhooksend = await fetch(webhook, init)
+  const webhookresponse = await webhooksend.json()
+  return new Response(JSON.stringify(webhookresponse))
+}
+
+addEventListener("fetch", event => {
+  return event.respondWith(handleRequest())
+})
